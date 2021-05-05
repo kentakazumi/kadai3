d1be34c6ff15c3bdae629b293c9f22eecd44b020
kentakazumi <majorkzmkkzmk@gmail.com>
2021-05-05 06:48:26 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     get 'followsings' => 'relationships#follows'
     get 'followers' => 'relationships#followers'
   end
+  get '/search', to: 'searchs#search'
 end
